812abcee7827d4063b8855cd1053b6920e064921
Your Name <jasonbataillon@gmail.com>
2025-01-16 22:30:12 -0500

    Finished creating routes and completed assignment.

--- api/users.js
+++ api/users.js
@@ -1,5 +1,51 @@
 const express = require('express');
 const router = express.Router();
 module.exports = router;
-
 const prisma = require('../prisma');
+
+router.get('/', async (req, res, next) => {
+  try {
+    const users = await prisma.user.findMany();
+    res.json(users);
+  } catch (e) {
+    next(e);
+  }
+});
+
+router.get('/:id', async (req, res, next) => {
+  const { id } = req.params;
+  try {
+    // We can throw an error instead of checking for a null user
+    const user = await prisma.user.findUnique({
+      where: { id: +id },
+      include: { playlists: true },
+    });
+    if (user) {
+      res.json(user);
+    } else {
+      next({ status: 404, message: `${id} user does not exist.` });
+    }
+  } catch (e) {
+    next(e);
+  }
+});
+
+router.post('/:id/playlists', async (req, res, next) => {
+  const { id } = req.params;
+  const { name, description } = req.body;
+  if (!name || !description) {
+    return next({
+      status: 400,
+      message: 'A name AND description must be provided.',
+    });
+  }
+  try {
+    // userId have been converted to numbers
+    const playlist = await prisma.playlist.create({
+      data: { name, description, ownerId: +id },
+    });
+    res.status(201).json(playlist);
+  } catch (e) {
+    next(e);
+  }
+});
